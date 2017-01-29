7dfe8d81f372c4da9bd4de3625baaf3bae1fb411
Vikas Patel <v.vikas.patel@gmail.com>
2017-01-29 18:11:28 +0530

    subscription form

--- app.js
+++ app.js
@@ -3,32 +3,40 @@ var app = express();
 var port = process.env.PORT || 3001;
 var router = express.Router();
 var path = require('path');
+var request = require('request');
+var bodyParser = require('body-parser');
+
 
 app.use(express.static(path.join(__dirname, 'views')))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded()); // get information from html forms
+
+router.post("/subscribe",function(req,res){
+  console.log(req.body);
+  request.post(
+		// Google Form: eRiyaz email subscription list
+		'https://docs.google.com/forms/d/1TxAR8sIKykkmJzOWssqhyjjPQ7jQPx6ZfFBAGjgrCuk/formResponse',
+		{form:req.body},
+		function (error, response, body) {
+			
+			if (!error && response.statusCode == 200) {
+				var json_resp = {"info":"Subscribed successfully."};
+				res.json(json_resp);
+			} else {
+				var json_resp = {};
+				res.statusCode = 400;
+				res.json(json_resp);
+			}
+		}
+	);
+});
 /*
-router.use(function (req,res,next) {
-  console.log("/" + req.method);
-  next();
-});
-
-router.get("/",function(req,res){
-  res.sendFile(path + "index.html");
-});
-
-router.get("/about",function(req,res){
-  res.sendFile(path + "about.html");
-});
-
 router.get("/contact",function(req,res){
   res.sendFile(path + "contact.html");
 });
-
+*/
 app.use("/",router);
 
-app.listen(3000,function(){
-  console.log("Live at Port 3000");
-});
-*/
 var server = app.listen(port, function() {
 	console.log('App listening at http://%s:%s', server.address().address, server.address().port);
 });
